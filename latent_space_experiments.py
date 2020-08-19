f16df47e50b344287faa11540a69d7defe148b9b
fbottarel <fabrizio.bottarel@iit.it>
2020-08-19 10:40:26 +0200

    added method to add noise to a pc and complete it

--- latent_space_experiments.py
+++ latent_space_experiments.py
@@ -12,7 +12,7 @@ import numpy as np
 from tqdm import tqdm
 
 # Hardcoded variables, because why not
-pcd_folder = '/home/fbottarel/workspace/PointSDF/pcs/pointclouds_435/processed_pointclouds'
+pcd_folder = '/home/fbottarel/workspace/PointSDF/pcs/rendered_ycb/processed_pcs'
 model_func = get_pointconv_model
 model_name = 'pointconv_mse_cf'
 model_path = '/home/fbottarel/workspace/PointSDF/models/reconstruction'
@@ -111,6 +111,66 @@ def mesh_disturb_embedding(model_func, model_path, save_path, pcd_folder):
 
             mise_voxel(get_sdf_embedding_query, bound, initial_voxel_resolution, final_voxel_resolution, voxel_size, centroid_diff, os.path.join(save_path, mesh + "_" + str(experiment_idx) + '.obj'), verbose=False)
 
+def mesh_noisy_pc(model_func, model_path, save_path, pcd_folder):
+    # Load point cloud, disturb it with some noise distribution and obtain embeddings + completions on it
+
+    # Setup model
+    _, get_embedding, _ , get_sdf_from_embedding = get_sdf_prediction(model_func, model_path)
+
+    # Get names of partial views.
+    import glob
+    meshes = [os.path.splitext(os.path.basename(filename))[0] for filename in glob.glob(pcd_folder + "**/*.pcd")]
+    
+    # Bounds of 3D space to evaluate in: [-bound, bound] in each dim.
+    bound = 0.8
+    # Starting voxel resolution.
+    initial_voxel_resolution = 16
+    # Final voxel resolution.
+    final_voxel_resolution = 64
+
+    # Parameters of the noise distribution
+    noise_mean = 0
+    noise_sigma_sq = 0.005
+
+    noise_mean_vector = np.ones((3)) * noise_mean
+    noise_cov_matrix = np.eye(3) * noise_sigma_sq
+
+    # How many times a single point cloud should be perturbed
+    number_of_experiments = 5
+
+    for mesh in tqdm(meshes):
+
+        # Point cloud for this view.
+        pc_, length, scale, centroid_diff = get_pcd(mesh, pcd_folder, object_frame=False, verbose=False)
+        
+        voxel_size = (2.*bound * length) / float(final_voxel_resolution)        
+
+        if pc_ is None:
+            print(mesh, " has no point cloud.")
+            continue
+
+        for experiment_idx in range(number_of_experiments):
+
+            # Perturb point cloud with additive gaussian noise
+            noise_samples = np.random.multivariate_normal(noise_mean_vector, noise_cov_matrix, pc_.shape[0])
+
+            if noise_samples.shape != pc_.shape:
+                print(mesh, " dimensions are not compatible with noise sample matrix.")
+                continue
+
+            pc_noisy_ = pc_ + noise_samples
+            point_clouds_ = np.reshape(pc_, (1,1000,3))
+
+            # Get embedding and save it
+            embedding = get_embedding(pc_noisy_)
+            embedding_filename = os.path.join(save_path, mesh + str(experiment_idx) + '_noisy_pc_embedding.npy')
+            np.save(embedding_filename, embedding)
+
+            def get_sdf_embedding_query(query_points):
+                return get_sdf_from_embedding(embedding, query_points)
+
+            mise_voxel(get_sdf_embedding_query, bound, initial_voxel_resolution, final_voxel_resolution, voxel_size, centroid_diff, os.path.join(save_path, mesh + "_" + str(experiment_idx) + '.obj'), verbose=False)
+
 
 if __name__ == "__main__":
     
@@ -127,11 +187,14 @@ if __name__ == "__main__":
     #     save_path=mesh_folder,
     #     pcd_folder=pcd_folder)
 
-    mesh_disturb_embedding(
+    # mesh_disturb_embedding(
+    #     model_func=model_func,
+    #     model_path=model_folder,
+    #     save_path=os.path.join(mesh_folder, 'noisy_meshes'),
+    #     pcd_folder=pcd_folder)
+
+    mesh_noisy_pc(
         model_func=model_func,
         model_path=model_folder,
-        save_path=os.path.join(mesh_folder, 'noisy_meshes'),
+        save_path=os.path.join(mesh_folder, 'meshes_noisy_pc'),
         pcd_folder=pcd_folder)
-
-    
-    
