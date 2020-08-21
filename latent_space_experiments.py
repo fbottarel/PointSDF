d0850c9be9ad0d01caccfc50b29835886473b18f
fbottarel <fabrizio.bottarel@iit.it>
2020-08-21 15:31:08 +0200

    added plot functions

--- latent_space_experiments.py
+++ latent_space_experiments.py
@@ -63,6 +63,126 @@ def draw_boxplot(data, edge_color, fill_color, labels, show=True):
 
     return bp
 
+def heatmap(data, row_labels, col_labels, ax=None,
+            cbar_kw={}, cbarlabel="", **kwargs):
+    """
+    Create a heatmap from a numpy array and two lists of labels.
+
+    Parameters
+    ----------
+    data
+        A 2D numpy array of shape (N, M).
+    row_labels
+        A list or array of length N with the labels for the rows.
+    col_labels
+        A list or array of length M with the labels for the columns.
+    ax
+        A `matplotlib.axes.Axes` instance to which the heatmap is plotted.  If
+        not provided, use current axes or create a new one.  Optional.
+    cbar_kw
+        A dictionary with arguments to `matplotlib.Figure.colorbar`.  Optional.
+    cbarlabel
+        The label for the colorbar.  Optional.
+    **kwargs
+        All other arguments are forwarded to `imshow`.
+    """
+
+    if not ax:
+        ax = plt.gca()
+
+    # Plot the heatmap
+    im = ax.imshow(data, **kwargs)
+
+    # Create colorbar
+    cbar = ax.figure.colorbar(im, ax=ax, **cbar_kw)
+    cbar.ax.set_ylabel(cbarlabel, rotation=-90, va="bottom")
+
+    # We want to show all ticks...
+    ax.set_xticks(np.arange(data.shape[1]))
+    ax.set_yticks(np.arange(data.shape[0]))
+    # ... and label them with the respective list entries.
+    ax.set_xticklabels(col_labels)
+    ax.set_yticklabels(row_labels)
+
+    # Let the horizontal axes labeling appear on top.
+    ax.tick_params(top=False, bottom=True,
+                   labeltop=False, labelbottom=True)
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+
+    # Turn spines off and create white grid.
+    for edge, spine in ax.spines.items():
+        spine.set_visible(False)
+
+    ax.set_xticks(np.arange(data.shape[1]+1)-.5, minor=True)
+    ax.set_yticks(np.arange(data.shape[0]+1)-.5, minor=True)
+    ax.grid(which="minor", color="w", linestyle='-', linewidth=3)
+    ax.tick_params(which="minor", bottom=False, left=False)
+
+    return im, cbar
+
+def annotate_heatmap(im, data=None, valfmt="{x:.2f}",
+                     textcolors=("black", "white"),
+                     threshold=None, **textkw):
+    """
+    A function to annotate a heatmap.
+
+    Parameters
+    ----------
+    im
+        The AxesImage to be labeled.
+    data
+        Data used to annotate.  If None, the image's data is used.  Optional.
+    valfmt
+        The format of the annotations inside the heatmap.  This should either
+        use the string format method, e.g. "$ {x:.2f}", or be a
+        `matplotlib.ticker.Formatter`.  Optional.
+    textcolors
+        A pair of colors.  The first is used for values below a threshold,
+        the second for those above.  Optional.
+    threshold
+        Value in data units according to which the colors from textcolors are
+        applied.  If None (the default) uses the middle of the colormap as
+        separation.  Optional.
+    **kwargs
+        All other arguments are forwarded to each call to `text` used to create
+        the text labels.
+    """
+
+    import matplotlib 
+
+    if not isinstance(data, (list, np.ndarray)):
+        data = im.get_array()
+
+    # Normalize the threshold to the images color range.
+    if threshold is not None:
+        threshold = im.norm(threshold)
+    else:
+        threshold = im.norm(data.max())/2.
+
+    # Set default alignment to center, but allow it to be
+    # overwritten by textkw.
+    kw = dict(horizontalalignment="center",
+              verticalalignment="center")
+    kw.update(textkw)
+
+    # Get the formatter in case a string is supplied
+    if isinstance(valfmt, str):
+        valfmt = matplotlib.ticker.StrMethodFormatter(valfmt)
+
+    # Loop over the data and create a `Text` for each "pixel".
+    # Change the text's color depending on the data.
+    texts = []
+    for i in range(data.shape[0]):
+        for j in range(data.shape[1]):
+            kw.update(color=textcolors[int(im.norm(data[i, j]) > threshold)])
+            text = im.axes.text(j, i, valfmt(data[i, j], None), **kw)
+            texts.append(text)
+
+    return texts
+
 def compute_distance_matrix(data):
     """Compute distance between every sample and all the others
 
@@ -403,18 +523,38 @@ def compute_dispersion_multi_pose_no_noise(model_func, model_path, save_path, pc
         embeddings = np.append(embeddings, embedding, axis=0)
 
         # Record pc class
-        object_labels.append[pc_name[:-8]]
+        object_labels.append(pc_name[3:-8])
 
-    distance_matrx = compute_distance_matrix(embeddings)
+    distance_matrix = compute_distance_matrix(embeddings)
+
+    # Draw the distance matrix
+    # fig, ax = plt.subplots()
+    # ax.matshow(distance_matrix)
+    # ax.set_xticks(np.arange(distance_matrix.shape[1]))
+    # ax.set_yticks(np.arange(distance_matrix.shape[0]))
+    # ax.set_xticklabels(object_labels)
+    # ax.set_yticklabels(object_labels)
+    # ax.xaxis.set_ticks_position('bottom')
+    # plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+    #         rotation_mode="anchor")
+    # plt.setp(ax.get_yticklabels(), ha="right",
+    #             rotation_mode="anchor")
+
+    # for i in range(distance_matrix.shape[0]):
+    #     for j in range(distance_matrix.shape[1]):
+    #         text = ax.text(j, i, '%.1f'%(distance_matrix[i, j]), ha="center", va="center", color="w")
     
-    # TODO from here
-
+    # ax.set_title('Distance matrix')
     
+    fig, ax = plt.subplots()
 
+    im, cbar = heatmap(distance_matrix, object_labels, object_labels, ax=ax,  cmap="YlGn", cbarlabel="L2 Distance in latent space")
+    
+    texts = annotate_heatmap(im, valfmt="{x:.1f}")
 
-
-
-
+    ax.set_title('Distance matrix')
+    fig.tight_layout()
+    plt.show()
 
 
 if __name__ == "__main__":
@@ -449,6 +589,12 @@ if __name__ == "__main__":
         model_path=model_folder,
         save_path='/home/fbottarel/workspace/PointSDF/latent_space_exp',
         pcd_folder=pcd_folder)
+
+    # compute_dispersion_multi_pose_no_noise(
+    #     model_func=model_func,
+    #     model_path=model_folder,
+    #     save_path='/home/fbottarel/workspace/PointSDF/latent_space_exp',
+    #     pcd_folder=pcd_folder)
 
     
     
